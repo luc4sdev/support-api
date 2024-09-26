cae0f166e34ff56a753af46d34888b1956ab6768
lucas <lucas.pfranca01@gmail.com>
2024-09-25 23:53:54 -0300

    feat: create tmp folder

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,20 @@
 import { app } from "./app";
 import { env } from "./env";
+import fs from 'fs';
+import path from 'path';
+
+
+const tmpDir = path.join(__dirname, '../', 'tmp');
+
+
+if (!fs.existsSync(tmpDir)) {
+    fs.mkdirSync(tmpDir, { recursive: true });
+    console.log('Pasta tmp criada na raiz do projeto.');
+}
 
 app.listen({
     host: '0.0.0.0',
     port: env.PORT,
 }).then(() => {
-    console.log('HTTP Server Running!')
-})
+    console.log('HTTP Server Running!');
+});
