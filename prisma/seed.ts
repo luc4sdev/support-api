904e4b3b430cc87b70cdedef375669f51f0236ec
lucas <lucas.pfranca01@gmail.com>
2024-09-27 00:27:01 -0300

    feat: add user create on seed

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,10 +1,14 @@
 import { faker } from '@faker-js/faker'
 import { PrismaClient } from '@prisma/client'
+import bcrypt from 'bcrypt';
 
 const prisma = new PrismaClient()
 
 async function seed() {
     await prisma.client.deleteMany()
+    await prisma.user.deleteMany()
+
+    await prisma.user.create({ data: { name: 'Admin', email: 'admin@email.com', password: await bcrypt.hash('admin123', 10) } })
 
     for (let i = 0; i <= 100; i++) {
         await prisma.client.create({
--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,6 @@
 import { app } from "./app";
 import { env } from "./env";
-import { prisma } from "./lib/prisma";
-import bcrypt from 'bcrypt';
 
-async function createUser() {
-    (await prisma.user.findMany()).length === 0 ? await prisma.user.create({ data: { name: 'Admin', email: 'admin@email.com', password: await bcrypt.hash('admin123', 10) } }) : () => { }
-}
-createUser()
 app.listen({
     host: '0.0.0.0',
     port: env.PORT,
